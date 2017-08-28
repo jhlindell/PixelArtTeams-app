4127186233ae22d2904d45448c994528ec095c68
Jon Lindell <jhlindell@yahoo.com>
2017-08-28 10:27:37 -0600

    basic landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class LandingPage extends Component {
+  render(){
+    return (
+      <div>Welcome to Pixel Art Teams!</div>
+    )
+  }
+}
+
+export default LandingPage;
