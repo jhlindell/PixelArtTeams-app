c2fb3240306dbda6deed941ad678e80050edd280
Jon Lindell <jhlindell@yahoo.com>
2017-08-30 14:09:26 -0600

    refactor to raise socket to app level

--- src/App.js
+++ src/App.js
@@ -4,11 +4,54 @@ import NavBar from './components/NavBar';
 import Grid from './components/Grid';
 import Palette from './components/Palette';
 import ProjectBox from './components/ProjectBox';
-import './App.css';
 import { Router, Route } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
+import {connect} from 'react-redux';
+import openSocket from 'socket.io-client';
+import { bindActionCreators } from 'redux';
+import './App.css';
+
+function pixelClick(x, y, color) {
+  return {
+    type: 'PIXEL_CLICK',
+    payload: { x, y, color }
+  }
+}
+
+function updateGrid(grid){
+  return {
+    type: 'UPDATE_GRID',
+    payload: grid
+  }
+}
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.sendPixelToSocket = this.sendPixelToSocket.bind(this);
+  }
+
+  componentDidMount() {
+    this.socket = openSocket('http://localhost:7000');
+    this.socket.on('connect', () => {
+      this.socket.emit('grid', 'update');
+    });
+    this.socket.on('pixel', (pixel) => {
+      this.props.pixelClick(pixel.x, pixel.y, pixel.color);
+    });
+    this.socket.on('gridUpdated', (grid)=> {
+      this.props.updateGrid(grid);
+    });
+  }
+
+  sendPixelToSocket(x, y, color){
+    this.socket.emit('pixel', {x: x, y: y, color: color});
+  }
+
+  componentWillUnmount() {
+    this.socket.disconnect();
+  }
+
   render() {
     return (
       <Router history={createHistory()}>
@@ -16,7 +59,7 @@ class App extends Component {
           <div><Route path="/" render={() => <NavBar />} /></div>
           <Row>
             <Col md="8">
-              <Route path="/" render={() => <Grid />} />
+              <Route path="/" render={() => <Grid sendPixel={this.sendPixelToSocket} />} />
               <Route path="/" render={() => <Palette />} />
             </Col>
             <Col md="4">
@@ -29,4 +72,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ pixelClick, updateGrid }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,56 +1,16 @@
 import React, {Component} from 'react';
 import Pixel from './Pixel';
 import {connect} from 'react-redux';
-import openSocket from 'socket.io-client';
-import { bindActionCreators } from 'redux';
 
-function pixelClick(x, y, color) {
-  return {
-    type: 'PIXEL_CLICK',
-    payload: { x, y, color }
-  }
-}
-
-function updateGrid(grid){
-  return {
-    type: 'UPDATE_GRID',
-    payload: grid
-  }
-}
 
 class Grid extends Component {
-  constructor(props) {
-    super(props)
-    this.sendPixelToSocket = this.sendPixelToSocket.bind(this);
-  }
-
-  componentDidMount() {
-    this.socket = openSocket('http://localhost:7000');
-    this.socket.on('connect', () => {
-      this.socket.emit('grid', 'update');
-    });
-    this.socket.on('pixel', (pixel) => {
-      this.props.pixelClick(pixel.x, pixel.y, pixel.color);
-    })
-    this.socket.on('gridUpdated', (grid)=> {
-      this.props.updateGrid(grid);
-    })
-  }
-
-  sendPixelToSocket(x, y, color){
-    this.socket.emit('pixel', {x: x, y: y, color: color});
-  }
-
-  componentWillUnmount() {
-    this.socket.disconnect();
-  }
 
   render() {
     return (
       <div>
         <div id="canvas">
           {this.props.grid.map((row, y) => {
-            return row.map((pixel, x) => <Pixel x={x} y={y} color={pixel} sendPixel={this.sendPixelToSocket}/>)
+            return row.map((pixel, x) => <Pixel x={x} y={y} color={pixel} sendPixel={this.props.sendPixel}/>)
           })}
         </div>
       </div>
@@ -62,8 +22,4 @@ function mapStateToProps(state) {
   return {grid: state.gridReducer}
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ pixelClick, updateGrid }, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Grid);
+export default connect(mapStateToProps, null)(Grid);
