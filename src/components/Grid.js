da549db11ef6bb4974ed0912014cccc86f8b4516
Jon Lindell <jhlindell@yahoo.com>
2018-02-01 13:18:31 -0700

    refactored Grid to remove project selector

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react';
 import Pixel from './Pixel';
 import {connect} from 'react-redux';
-import Project from './Project';
 import {bindActionCreators} from 'redux';
 import { mouseDownAction, mouseUpAction } from '../actions/index';
-import { getProjects } from '../actions/socketActions';
 
 class Grid extends Component {
-  componentWillMount(){
-    this.props.getProjects();
-  }
   render(){
     let xCoord = this.props.grid[0].length;
     let gridWidth = xCoord * 20;
@@ -20,62 +15,26 @@ class Grid extends Component {
     newStyle.width = gridWidth + 'px';
     newStyle.margin = 'auto';
 
-    if(this.props.auth){
-      return (
-        <div className="row">
-          <div className="col col-md-12">
-            {
-              this.props.currentProject === 0
-              ?
-              <div>
-                <h4 className="projectCardText mt-4">Select a project</h4>
-                {
-                this.props.projects.map(project => <Project
-                  key={project.project_id}
-                  project={project} />)
-                }
-              </div>
-              :
-              <div
-                style={newStyle}
-                onMouseDown={() => this.props.mouseDownAction()}
-                onMouseUp={() => this.props.mouseUpAction()}
-              >
-                {this.props.grid.map((row, y) => {
-                  return row.map((pixel, x) => <Pixel
-                    x={x} y={y}
-                    color={pixel} />);
-                })}
-              </div>
-            }
-          </div>
+    return (
+      <div className="row">
+        <div className="col col-md-12">
+          <div style={newStyle}
+              onMouseDown={() => this.props.mouseDownAction()}
+              onMouseUp={() => this.props.mouseUpAction()} >
+              {this.props.grid.map((row, y) => {
+                return row.map((pixel, x) => <Pixel
+                  x={x} y={y}
+                  color={pixel} />);
+              })}
+            </div>
         </div>
-      );
-    } else {
-      return (
-        <div className="row">
-          <div className="col col-md-12">
-            <h4 className="projectCardText mt-4">Please Log In</h4>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 };
 
-
-function mapStateToProps(state) {
-  return {
-    grid: state.gridReducer,
-    projects: state.projectsReducer,
-    currentProject: state.currentProject,
-    auth: state.auth.authenticated,
-    token: state.auth.token
-  };
-}
-
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ mouseDownAction, mouseUpAction, getProjects}, dispatch);
+  return bindActionCreators({ mouseDownAction, mouseUpAction }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Grid);
+export default connect(null, mapDispatchToProps)(Grid);
--- src/components/MainCanvas.js
+++ src/components/MainCanvas.js
@@ -3,18 +3,39 @@ import Menu from './Menu';
 import NavBar from './NavBar';
 import Grid from './Grid';
 import Palette from './Palette';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { getProjects } from '../actions/socketActions';
 
 class MainCanvas extends Component {
+  componentWillMount(){
+    this.props.getProjects();
+  }
+
   render(){
     return (
       <div>
         <NavBar />
         <Menu />
         <Palette />
-        <Grid />
+        {this.props.currentProject !== 0 && <Grid grid={this.props.grid}/>}
       </div>
     )
   }
 }
 
-export default MainCanvas;
+function mapStateToProps(state) {
+  return {
+    grid: state.gridReducer,
+    projects: state.projectsReducer,
+    currentProject: state.currentProject,
+    auth: state.auth.authenticated,
+    token: state.auth.token
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getProjects}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainCanvas);
