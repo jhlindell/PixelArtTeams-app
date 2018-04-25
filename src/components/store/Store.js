b2f6b9ad33b2736ade1342462eac746ea4e6d10a
Jon Lindell <jhlindell@yahoo.com>
2018-04-25 12:21:35 -0600

    changed card testing messages

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -42,11 +42,11 @@ class Store extends Component {
     return (
       <div className="card" style={cardStyle}>
         {!this.props.stripeMessage && !this.state.showCheckout && <div>
-          <h4>Store development is in a future feature release. If you feel that your life has been immesurably enriched by this app and you want to give us money to show your eternal gratitude, click the button below.</h4>
-          <button className="btn btn-primary mt-3" onClick={()=> this.changeShowState()}>I Want to Give You Money</button>
+          <h4>Store development is in a future feature release. If you feel that your life has been immesurably enriched by this app and you want to give us money to show your eternal gratitude, click the button below. (note: credit card processing isn't live. This is just a test.)</h4>
+          <button className="btn btn-primary mt-3" onClick={()=> this.changeShowState()}>I'd Love to Donate!</button>
         </div>}
         {!this.props.stripeMessage && this.state.showCheckout && <div>
-          <h4>Donations are greatly appreciated. To proceed, please enter the donation amount and click the button below to process with Stripe.</h4>
+          <h4>Donations are greatly appreciated. To proceed, please enter the donation amount and click the button below to process with Stripe. (for testing, use card 4242 4242 4242 4242)</h4>
           <div style={columnStyle} className="mt-4">
             <CurrencyFormat className="mb-2" style={{textAlign: 'right'}} value={this.state.formattedAmount} thousandSeparator={true} prefix={'$'} fixedDecimalScale={true} decimalScale={0} onValueChange={(values) => {
                 const {formattedValue, value} = values;
